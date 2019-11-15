c3e57dbd98a3be3d283658b24bb07cc505d9ca53
Heondo Kim <heondokim@gmail.com>
2019-11-14 16:23:42 -0800

    just hasing stuff and figuring out postgres

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,22 @@
-console.log('listening');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+const usersRoute = require('./routes/users');
+
+app.use('/api/users', usersRoute);
+
+app.use((err, req, res, next) => {
+  res.send({
+    type: err.type,
+    error: err.message
+  });
+});
+
+const port = process.env.PORT;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log('listening on port ' + port);
+});
